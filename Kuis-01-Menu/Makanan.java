a5af62812e2210e3c22bb5880c70bbdc2a195873
Munawar A <munawar.a21895@gmail.com>
2015-10-10 16:14:24 +0700

    kuis

---
+++ Kuis-01-Menu/Makanan.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Makanan here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Makanan 
+{
+    JenisMakanan jenismakanan[];
+    
+    String nama,harga;
+    
+    public Makanan (String nama, String harga, JenisMakanan jenismakanan[]){
+        this.nama=nama;
+        this.harga=harga;
+        this.jenismakanan=jenismakanan;      
+    }
+}
